82ee7a5f151cdbbcb079b15f0dd5bca842c8bb51
Tim Hall <tim.hall.engr@gmail.com>
2017-10-01 21:54:57 -0400

    Cleanup

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  projects: ['packages/*'],
-  testRegex: '\\.(test|spec)\\.(ts|js)$',
-  transform: {
-    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js'
-  }
-};
