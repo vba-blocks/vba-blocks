cf2fae1ea6755c9757df31bdf82272a3021c83fb
Tim Hall <tim.hall.engr@gmail.com>
2018-04-02 17:28:33 -0400

    Remove cases for e2e

--- packages/cli/e2e/cli.e2e.js
+++ packages/cli/e2e/cli.e2e.js
@@ -1,15 +1,9 @@
 const { resolve } = require('path');
 const execute = require('./execute');
 
-const cases = {
-  build: { command: 'build' }
-};
+test('build', async () => {
+  const cwd = resolve(__dirname, `./${name}`);
+  const result = await execute(cwd, 'build');
 
-for (const [name, { command }] of Object.entries(cases)) {
-  test(name, async () => {
-    const cwd = resolve(__dirname, `./${name}`);
-    const result = await execute(cwd, command);
-
-    expect(result).toMatchSnapshot();
-  });
-}
+  expect(result).toMatchSnapshot();
+});
