58e2a612a3f2d7f17fac291b9033b77ffec75e05
Tim Hall <tim.hall.engr@gmail.com>
2018-04-26 21:05:43 -0400

    Remove desktop

--- packages/desktop/static/bin/vba-blocks.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-set ELECTRON_RUN_AS_NODE=1
-echo %time%
-call "%~dp0..\vba-blocks.exe" "%~dp0\vba-blocks.js" %*
-echo %time%
-endlocal
--- packages/desktop/static/bin/vba-blocks.js
+++
@@ -1,4 +0,0 @@
-const { resolve } = require('path');
-const archive = resolve('../resources/app.asar');
-
-require(`${archive}/node_modules/vba-blocks/bin/vba-blocks`);
