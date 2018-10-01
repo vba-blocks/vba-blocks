19c5994d4b42f106ce35b581e271e51fd15a8a40
Tim Hall <tim.hall.engr@gmail.com>
2018-10-01 10:13:08 -0400

    Add timing to build

--- src/bin/vba-blocks-build.ts
+++ src/bin/vba-blocks-build.ts
@@ -1,5 +1,6 @@
 import { Args } from 'mri';
 import dedent from 'dedent';
+import time from 'pretty-hrtime';
 import build from '../actions/build';
 import open from '../utils/open';
 
@@ -9,7 +10,7 @@ const help = dedent`
   Create a clean build of the project (after backing up any existing built targets).
 
   Usage: vba-blocks build [options]
-  
+
   Options:
     --target=TYPE   Build target of type TYPE
     --open          Open built target`;
@@ -20,10 +21,12 @@ module.exports = async (args: Args) => {
     return;
   }
 
+  const start = process.hrtime();
   const target = <string | undefined>args.target;
   const addin = <string | undefined>args.addin;
 
   const path = await build({ target, addin });
+  console.log(`Done. ${time(process.hrtime(start))}`);
 
   if (!!args.open) {
     open(path);
---
+++ src/typings/pretty-hrtime.d.ts
@@ -0,0 +1,8 @@
+declare module 'pretty-hrtime' {
+  export interface Options {}
+
+  export default function prettyHrtime(
+    source: number[],
+    options?: Options
+  ): string;
+}
