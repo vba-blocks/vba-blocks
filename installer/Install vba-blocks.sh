f53ddca12dba16a1e5af83c70957c2809b0af997
Tim Hall <tim.hall.engr@gmail.com>
2018-10-16 15:22:20 -0400

    Finish install script for Mac

---
+++ installer/Install vba-blocks.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(dirname "$0")
+SRC=$BASE_DIR"/../dist/vba-blocks.app"
+APP="/Applications/vba-blocks.app"
+
+# Remove existing vba-blocks.app
+if [ -d $APP ]; then
+  echo "Removing previous version at "$APP
+  rm -rf "$APP"
+fi
+
+# Copy vba-blocks.app to Applications
+echo "Copying "$SRC" to "$APP
+cp -rf "$SRC" "$APP"
+
+# Add bin to .profile / .bash_profile
+BIN=$APP"/bin"
+VBA_BLOCKS="export PATH=\"\$PATH:"$BIN"\""
+PROFILE=$HOME"/.profile"
+BASH_PROFILE=$HOME"/.bash_profile"
+
+if ! [ -a $PROFILE ] || ! grep -q $BIN $PROFILE; then
+  echo "Adding "$VBA_BLOCKS" to "$PROFILE
+  echo $VBA_BLOCKS >> "$PROFILE"
+fi
+if [ -a $BAS_PROFILE ] && ! grep -q $BIN $BASH_PROFILE; then
+  echo "Adding "$VBA_BLOCKS" to "$BASH_PROFILE
+  echo $VBA_BLOCKS >> "$BASH_PROFILE"
+fi
+
+# Create .vba-blocks folder
+CACHE=$HOME"/.vba-blocks"
+ADDINS_SRC=$APP"/addins/"
+ADDINS_LINK=$HOME"/vba-blocks Add-ins"
+
+if ! [ -d $CACHE ]; then
+  mkdir "$CACHE"
+fi
+
+echo $ADDINS_SRC" -> "$ADDINS_LINK
+ln -sf "$ADDINS_SRC" "$ADDINS_LINK"
