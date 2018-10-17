74f140b400254870b716e2536c761b3ebd9f6178
Tim Hall <tim.hall.engr@gmail.com>
2018-10-16 21:24:54 -0400

    Add rcedit

---
+++ scripts/rcedit.js
@@ -0,0 +1,25 @@
+const { promisify } = require('util');
+const { join } = require('path');
+const rcedit = promisify(require('rcedit'));
+const { version } = require('../package.json');
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
+
+async function main() {
+  const exe = join(__dirname, '../dist/unpacked/bin/vba-blocks.exe');
+  await rcedit(exe, {
+    'version-string': {
+      ProductName: 'vba-blocks',
+      FileDescription: 'vba-blocks CLI',
+      CompanyName: 'vba-blocks',
+      LegalCopyright: `Copyright ${new Date().getFullYear()}, Tim Hall`,
+      OriginalFilename: 'vba-blocks.exe'
+    },
+    'file-version': version,
+    'product-version': version
+    // icon: 'TODO'
+  });
+}
