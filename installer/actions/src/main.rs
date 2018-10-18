beeff26f264c79d9a6f259e59e835c5df765dd49
Tim Hall <tim.hall.engr@gmail.com>
2018-10-18 09:30:40 -0400

    Log (and swallow) errors in installer actions

--- installer/actions/src/main.rs
+++ installer/actions/src/main.rs
@@ -6,29 +6,62 @@ extern crate winreg;
 
 mod registry;
 
-use std::path::Path;
-use std::fs::{remove_file, read_link};
 use std::env;
+use std::fs::{remove_file, read_link, OpenOptions};
+use std::io::Write;
+use std::panic;
+use std::path::Path;
 use symlink::symlink_file;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let addins_dir = Path::new(&dirs::home_dir().expect("Couldn't find homedir"))
-        .join("AppData").join("Roaming").join("Microsoft").join("Addins");
+    let result = panic::catch_unwind(|| {
+        let args: Vec<String> = env::args().collect();
+        let addins_dir = Path::new(&dirs::home_dir().expect("Couldn't find homedir"))
+            .join("AppData").join("Roaming").join("Microsoft").join("Addins");
 
-    if args[1] == "--install" {
-        let install_dir = Path::new(&args[2]);
+        if args[1] == "--install" {
+            let install_dir = Path::new(&args[2]);
 
-        // Excel
-        ensure_file_symlink(
-            install_dir.join("addins").join("vba-blocks.xlam").as_path(),
-            addins_dir.join("vba-blocks.xlam").as_path()
-        );
-        registry::add_to_open("Excel", "vba-blocks.xlam");
-        registry::enable_vbom("Excel");
-    } else {
-        registry::remove_from_open("Excel", "vba-blocks.xlam");
-        remove_symlink(addins_dir.join("vba-blocks.xlam").as_path());
+            // Excel
+            ensure_file_symlink(
+                install_dir.join("addins").join("vba-blocks.xlam").as_path(),
+                addins_dir.join("vba-blocks.xlam").as_path()
+            );
+            registry::add_to_open("Excel", "vba-blocks.xlam");
+            registry::enable_vbom("Excel");
+        } else {
+            registry::remove_from_open("Excel", "vba-blocks.xlam");
+            remove_symlink(addins_dir.join("vba-blocks.xlam").as_path());
+        }
+    });
+
+    if let Err(cause) = result {
+        let message = if let Some(e) = cause.downcast_ref::<&'static str>() {
+            format!("Installation failed: {}", e)
+        } else {
+            "Installation failed with an unknown error".to_string()
+        };
+        eprintln!("{}", message);
+
+        let mut path = env::temp_dir();
+        path.push("vba-blocks.log");
+
+        let mut log = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(&path);
+
+        if let Err(e) = log {
+            eprintln!("Log failed: {:?}", e);
+            return;
+        }
+
+        if let Err(e) = writeln!(log.unwrap(), "{}", message) {
+            eprintln!("Write log failed: {:?}", e);
+            return;
+        } else {
+            eprintln!("Log written to {:?}", path);
+        }
     }
 }
 
