528ad3d8eb9c44be67e1969373bd616032b29b0c
Tim Hall <tim.hall.engr@gmail.com>
2019-01-27 22:54:16 -0500

    Tweak dependencies

--- e2e.config.js
+++ e2e.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js', 'json'],
-  testRegex: '\\.e2e\\.ts$',
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  }
+  testRegex: '\\.e2e\\.ts$'
 };
