8652bd6d6fb2ab88f1fe93a833743e7302b3ee9d
Tim Hall <tim.hall.engr@gmail.com>
2019-01-30 17:59:36 -0500

    has - Handle objects with null prototype

--- src/utils/has.ts
+++ src/utils/has.ts
@@ -1,3 +1,3 @@
 export default function has(value: object | undefined | null, key: string): boolean {
-  return !!value && typeof value.hasOwnProperty === 'function' && value.hasOwnProperty(key);
+  return !!value && Object.hasOwnProperty.call(value, key);
 }
