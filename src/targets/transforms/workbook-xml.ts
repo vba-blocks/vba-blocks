b66cb3fd160b965aa6e2f842e16967ab0dbfff4c
Tim Hall <tim.hall.engr@gmail.com>
2019-04-03 10:57:02 -0400

    More functional approach

--- src/targets/transforms/workbook-xml.ts
+++ src/targets/transforms/workbook-xml.ts
@@ -1,11 +1,14 @@
+import { parseXml, convertXml } from '../../utils/xml';
 import { UnzipFile } from '../../utils/zip';
 
 const WORKBOOK_XML = /xl[\/,\\]workbook\.xml/i;
 
-export default {
-  match: (file: UnzipFile) => WORKBOOK_XML.test(file.path),
-  apply: (data: Buffer) => {
-    // TODO
-    return data;
-  }
-};
+export default function transformWorkbookXml(file: UnzipFile): UnzipFile {
+  if (!WORKBOOK_XML.test(file.path)) return file;
+
+  const xml = parseXml(file.data.toString('utf8'));
+  // TODO
+
+  file.data = Buffer.from(convertXml(xml));
+  return file;
+}
