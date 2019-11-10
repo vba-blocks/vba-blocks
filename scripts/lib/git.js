03851436c244b5df70eb3b5376ce3df69ac8eab3
Tim Hall <tim.hall.engr@gmail.com>
2019-11-09 23:26:24 -0500

    Use local registry and commit and push in publish

--- scripts/lib/git.js
+++ scripts/lib/git.js
@@ -1,6 +1,8 @@
 const { promisify } = require('util');
 const execFile = promisify(require('child_process').execFile);
 
+const remote = 'https://github.com/vba-blocks/registry.git';
+
 module.exports = {
   async pull({ dir }) {
     try {
@@ -9,20 +11,32 @@ module.exports = {
       throw new Error(`git pull failed in "${dir}". ${error.message}`);
     }
   },
+
   async commit({ dir, message }) {
     try {
-      await execFile('git', ['add', '-A']);
+      await execFile('git', ['add', '-A'], { cwd: dir });
+      await execFile('git', ['commit', '-am', message], { cwd: dir });
     } catch (error) {
       throw new Error(
         `git add -A && git commit -am "${message}" failed in "${dir}". ${error.message}`
       );
     }
   },
+
   async push({ dir }) {
     try {
-      await execFile('git', ['push']);
+      await execFile('git', ['push'], { cwd: dir });
     } catch (error) {
       throw new Error(`git push failed in "${dir}". ${error.message}`);
+    }
+  },
+
+  async clone({ dir }) {
+    try {
+      console.log(`git clone ${remote} registry`, dir);
+      await execFile('git', ['clone', remote, 'registry'], { cwd: dir });
+    } catch (error) {
+      throw new Error(`git clone failed in "${dir}". ${error.message}`);
     }
   }
 };
